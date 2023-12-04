71574cedcbeac8121d361d68b279dab03833226d
101 <tmddnjs001133@dsm.hs.kr>
2023-12-04 19:00:03 +0900

    set

---
+++ src/main/kotlin/com/example/kotlinwithwebflux/KotlinWithWebFluxApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinwithwebflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinWithWebFluxApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinWithWebFluxApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinwithwebflux/KotlinWithWebFluxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinwithwebflux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinWithWebFluxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
